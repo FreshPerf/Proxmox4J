6c02577e7bae8ace24af9359edb76c4f2d71344b
Thibeau B. <35635613+w8fyz@users.noreply.github.com>
2026-01-06 12:00:53 +0100

    Refactor PveNode from record to class with cached fields
    Converted PveNode from a record to a class and added private final fields for PveQemu, PveLxc, and PveStorage, initializing them in the constructor. This change avoids creating new instances on each getter call and improves performance by caching these objects.

--- src/main/java/fr/freshperf/proxmox4j/entities/nodes/node/PveNode.java
+++ src/main/java/fr/freshperf/proxmox4j/entities/nodes/node/PveNode.java
@@ -1,6 +1,7 @@
 package fr.freshperf.proxmox4j.entities.nodes.node;
 
 import com.google.gson.reflect.TypeToken;
+import fr.freshperf.proxmox4j.entities.nodes.PveNodes;
 import fr.freshperf.proxmox4j.entities.nodes.node.qemu.PveQemu;
 import fr.freshperf.proxmox4j.entities.nodes.node.lxc.PveLxc;
 import fr.freshperf.proxmox4j.entities.nodes.node.storage.PveStorage;
@@ -9,7 +10,23 @@ import fr.freshperf.proxmox4j.request.ProxmoxRequest;
 
 import java.util.List;
 
-public record PveNode(ProxmoxHttpClient client, String nodeName) {
+public class PveNode {
+
+    private final ProxmoxHttpClient client;
+    private final String nodeName;
+
+    private final PveQemu pveQemu;
+    private final PveLxc pveLxc;
+    private final PveStorage pveStorage;
+
+    public PveNode(ProxmoxHttpClient client, String nodeName) {
+        this.client = client;
+        this.nodeName = nodeName;
+
+        this.pveQemu = new PveQemu(client, nodeName);
+        this.pveLxc = new PveLxc(client, nodeName);
+        this.pveStorage = new PveStorage(client, nodeName);
+    }
 
     public ProxmoxRequest<List<PveNodeIndex>> getIndex() {
         return new ProxmoxRequest<>(() -> 
@@ -30,15 +47,15 @@ public record PveNode(ProxmoxHttpClient client, String nodeName) {
     }
 
     public PveQemu getQemu() {
-        return new PveQemu(client, nodeName);
+        return pveQemu;
     }
 
     public PveLxc getLxc() {
-        return new PveLxc(client, nodeName);
+        return pveLxc;
     }
 
     public PveStorage getStorage() {
-        return new PveStorage(client, nodeName);
+        return pveStorage;
     }
 }
 
